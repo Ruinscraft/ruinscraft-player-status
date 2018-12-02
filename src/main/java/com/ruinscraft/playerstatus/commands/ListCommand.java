d6460ddd012e509bedd65aeb464c48250bb176e0
Dexton Anderson <contact@dextonanderson.com>
2018-12-02 11:45:03 -0600

    prettify list command

--- src/main/java/com/ruinscraft/playerstatus/commands/ListCommand.java
+++ src/main/java/com/ruinscraft/playerstatus/commands/ListCommand.java
@@ -1,5 +1,8 @@
 package com.ruinscraft.playerstatus.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -18,14 +21,24 @@ public class ListCommand implements CommandExecutor {
 			/* This is actually run sync, it is always cached */
 			Multimap<String, String> players = PlayerStatusPlugin.getAPI().getOnline();
 
+			List<String> foundStaff = new ArrayList<>();
+			
 			for (String server : players.keySet()) {
 				if (!sender.hasPermission("server." + server)) {
 					continue;
 				}
 
-				sender.sendMessage(server + ":");
-
+				String serverName = ChatColor.GOLD + server + ChatColor.YELLOW + " (" + players.get(server).size() + ")" + ChatColor.GOLD + ": ";
+				List<String> serverPlayers = new ArrayList<>();
+				
+				int maxPlayers = 25;
+				
 				for (String player : players.get(server)) {
+					if (maxPlayers-- == 1) {
+						serverPlayers.add(ChatColor.GRAY + " and " + (players.get(server).size() - 25) + " more");
+						break;
+					}
+					
 					boolean vip = PlayerStatusPlugin.getVaultPermissions().playerHas(null, Bukkit.getOfflinePlayer(player), "group.vip");
 					boolean builder = PlayerStatusPlugin.getVaultPermissions().playerHas(null, Bukkit.getOfflinePlayer(player), "group.builder");
 					boolean helper = PlayerStatusPlugin.getVaultPermissions().playerHas(null, Bukkit.getOfflinePlayer(player), "group.helper");
@@ -34,32 +47,43 @@ public class ListCommand implements CommandExecutor {
 					boolean owner = PlayerStatusPlugin.getVaultPermissions().playerHas(null, Bukkit.getOfflinePlayer(player), "group.owner");
 
 					if (owner) {
-						player = ChatColor.DARK_RED + player;
+						serverPlayers.add(ChatColor.DARK_RED + player);
+						foundStaff.add(ChatColor.DARK_RED + player);
 					}
 
 					else if (admin) {
-						player = ChatColor.GOLD + player;
+						serverPlayers.add(ChatColor.GOLD + player);
+						foundStaff.add(ChatColor.GOLD + player);
 					}
 
 					else if (mod) {
-						player = ChatColor.BLUE + player;
+						serverPlayers.add(ChatColor.BLUE + player);
+						foundStaff.add(ChatColor.BLUE + player);
 					}
 
 					else if (helper) {
-						player = ChatColor.AQUA + player;
+						serverPlayers.add(ChatColor.AQUA + player);
+						foundStaff.add(ChatColor.AQUA + player);
 					}
 					
 					else if (builder) {
-						player = ChatColor.GREEN + player;
+						serverPlayers.add(ChatColor.GREEN + player);
 					}
 					
 					else if (vip) {
-						player = ChatColor.DARK_PURPLE + player;
+						serverPlayers.add(ChatColor.DARK_PURPLE + player);
+					} 
+					
+					else {
+						serverPlayers.add(ChatColor.GRAY + player);
 					}
-
-					sender.sendMessage(player);
 				}
+				
+				sender.sendMessage(serverName + String.join(", ", serverPlayers));
 			}
+			
+			sender.sendMessage(ChatColor.GOLD + "Staff online " + ChatColor.YELLOW + "(" + foundStaff.size() + ")" + ChatColor.GOLD + ": " + String.join(", ", foundStaff));
+			sender.sendMessage(ChatColor.GOLD + "Total players online: " + ChatColor.YELLOW + PlayerStatusPlugin.getAPI().getOnlyPlayers().size());
 		});
 
 		return true;
