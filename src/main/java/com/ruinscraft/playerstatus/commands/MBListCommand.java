19a897fd081363bedc0ca4b32f43850b5afdad00
Dexton Anderson <contact@dextonanderson.com>
2019-07-20 20:29:28 -0500

    run mblist async

--- src/main/java/com/ruinscraft/playerstatus/commands/MBListCommand.java
+++ src/main/java/com/ruinscraft/playerstatus/commands/MBListCommand.java
@@ -16,22 +16,23 @@ public class MBListCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Multimap<String, String> players = PlayerStatusPlugin.get().getAPI().getOnline();
+        PlayerStatusPlugin.get().getServer().getScheduler().runTaskAsynchronously(PlayerStatusPlugin.get(), () -> {
+            Multimap<String, String> players = PlayerStatusPlugin.get().getAPI().getOnline();
+            List<String> mbers = new ArrayList<>();
 
-        List<String> mbers = new ArrayList<>();
+            for (String server : players.keySet()) {
+                for (String username : players.get(server)) {
+                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(username);
+                    boolean is_mber = PlayerStatusPlugin.getVaultPerms().playerHas(Bukkit.getWorlds().get(0).getName(), offlinePlayer, "group.mb");
 
-        for (String server : players.keySet()) {
-            for (String username : players.get(server)) {
-                OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(username);
-                boolean is_mber = PlayerStatusPlugin.getVaultPerms().playerHas(Bukkit.getWorlds().get(0).getName(), offlinePlayer, "group.mb");
-
-                if (is_mber) {
-                    mbers.add(username);
+                    if (is_mber) {
+                        mbers.add(username);
+                    }
                 }
             }
-        }
 
-        sender.sendMessage(ChatColor.GREEN + "[MBLIST]" + ChatColor.RED + "(" + mbers.size() + ") " + String.join(", ", mbers));
+            sender.sendMessage(ChatColor.GREEN + "[mblist]" + ChatColor.RED + " (" + mbers.size() + ") " + String.join(", ", mbers));
+        });
 
         return true;
     }
